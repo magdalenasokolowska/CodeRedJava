e192b9e2aa0f10f284ee71da8a36005fc618cec5
Magda <magdalena.e.sokolowska@gmail.com>
2022-10-27 10:23:10 +0200

    another excercse

--- src/weekDays.java
+++ src/weekDays.java
@@ -4,19 +4,32 @@ public class weekDays {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Give number from 1 to 7");
-
         int number = console.nextInt();
 
-        if (number == 1) {
-            System.out.println("Monday");
-        } else if (number == 2) {
-            System.out.println("Tuesday");
+        switch (number) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+            default:
+                System.out.println("Your number is invalid");
+        }
 
-        } else if (number == 3) {
-            System.out.println("Wednesday");
-        } else if (number == 4) {
-            System.out.println("Thursday");
-        } else
-            System.out.println("Friday");
     }
 }
