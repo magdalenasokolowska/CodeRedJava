900a7b659aa8f1d6debc129e6b1e7024f181d46a
Magda <magdalena.e.sokolowska@gmail.com>
2022-11-17 09:55:06 +0100

    arrays

---
+++ src/ArraysLoops.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArraysLoops {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Give me three numbers");
+        int[] tablicaInt = new int[3];
+        tablicaInt[0] = console.nextInt();
+        tablicaInt[1] = console.nextInt();
+        tablicaInt[2] = console.nextInt();
+
+
+
+        System.out.println(tablicaInt[2]);
+
+
+
+
+    }
+}
