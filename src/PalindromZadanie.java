198a54a7703a1d239886cd1292fc21c33ceb9ac8
Magda <magdalena.e.sokolowska@gmail.com>
2022-11-24 10:21:13 +0100

    loops zadania

---
+++ src/PalindromZadanie.java
@@ -0,0 +1,32 @@
+public class PalindromZadanie {
+
+    public static void main(String[] args) {
+
+        /*String word = "racecar";
+
+        char[] converted = word.toCharArray();
+
+        for (int i = converted.length - 1; i >=0; i--)
+            System.out.println(converted[i]);
+            do porownania equals
+            */
+
+        var text = "racecar";
+        if (isPalindrome(text)) {
+            System.out.println(text + " is palindrome");
+        } else {
+            System.out.println(text + " is not a palindrome");
+        }
+    }
+
+        private static boolean isPalindrome(String text) {
+            var len = text.length();
+            for (int i =0; i <= len / 2; i++){
+                if (text.charAt(i) != text.charAt(len - i - 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
